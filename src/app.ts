5f51a12386e3700683d1b2d746f0677c7265c50b
jw030728 <jw1853063@gmail.com>
2021-08-22 12:57:29 +0900

    html parser로 변경

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
-import { Crawler } from "./libs/crawler";
+import { CrawlerCoordinator } from "./libs/crawlerCoordinator";
+import { parse } from "node-html-parser";
+
+// const text = `<body>
+// <a href="https://naver.com">hello</a>
+// <div>sdfdsfsf</div>
+// <a href="https://kakao.com">world</a>
+// </body>`;
+
+// const matched = text.match(/<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1>/i);
+// console.log(matched);
+// const multipleMatched = text.match(/<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1>/g);
+// console.log(multipleMatched);
+
+// const html = parse(text);
+// console.log(html.querySelector("a"));
 
 (async () => {
-    //주어진 주소로 탐색 
-    const crawler = new Crawler("http://naver.com/dszfdghfjgkhl");
-    //crawler.trip()을 비동기 처리
-    await crawler.trip();
+    const coordinator = new CrawlerCoordinator();
+    coordinator.reportUrl("http://news.naver.com");
+    await coordinator.start();
 })();
--- src/libs/crawler.ts
+++ src/libs/crawler.ts
@@ -1,19 +1,25 @@
 import axios, { AxiosError } from "axios";
+import { CrawlerCoordinator } from "./crawlerCoordinator";
+import { parse } from "node-html-parser";
 
 export class Crawler {
     private url: string;
     private content?: string;
+    private coordinator: CrawlerCoordinator;
+    private host?: string;
 
-    public constructor(url: string) {
+    public constructor(url: string, coordinator: CrawlerCoordinator) {
         this.url = url;
+        this.coordinator = coordinator;
     }
 
-    //처리되면 문자열|null을 줌
+    //처리되면 문자열 or null을 줌
     private async fetch(): Promise<string | null> {
         try {
-            const { data } = await axios.get(this.url, {
+            const { data, request } = await axios.get(this.url, {
                 timeout: 3000,  //시간제한 3초
             });
+            this.host = request.host;
             return data; //html로 받아서 data는 string
         }
         catch (error) {
@@ -27,15 +33,46 @@ export class Crawler {
     }
 
     public async trip(): Promise<void> {
-        //내가 원하는 값을 줄지 안줄지 기다리는중 await
+        //await 하던일을 계속 기다리기만하면 다른걸 하도록 해주는거
         const result = await this.fetch();
         if (result) {
-            console.log(result);
             this.content = result;
+            await this.parseContent();
         }
         else {
             console.log("Failed to get url data");
-
         }
+    }
+
+    //anchors테이블에 a tag에서 href속성값(링크) 빼오기
+    private async parseContent(): Promise<void> {
+        if (!this.content) {
+            return;
+        }
+        //a tag찾기
+        const html = parse(this.content);
+        const anchors = html.querySelectorAll("a");
+
+        anchors.forEach((anchor) => {
+            const href = anchor.getAttribute('href');//href속성값 
+            if (!href) {
+                return;
+            }
+            const matched = href.match(
+                /^(((http|ftp|https):\/\/)|(\/))*[\w-]+(\.[\w-]+)*([\w.,@?^=%& amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?/i
+            );
+            if (!matched) {
+                return null;
+            }
+
+            let url = matched[0];
+
+            if (url.startsWith("/")) url = this.host + url;
+            else if (!href.startsWith("http")) url = this.host + "/" + url;
+            //console.log(url);
+
+            // this.coordinator.reportUrl(url);
+        });
+        console.log(html.text.replace(/\s{2,}/g, " "));
     }
 }
